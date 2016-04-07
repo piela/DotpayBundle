220f158ecefe7a69da33b9ac772f6f604d7f4bd0
Krzysiek Piasecki <krzysiekpiasecki@gmail.com>
2016-04-07 20:44:17 +0200

    Register SettingsCompilerPass

--- src/SymfonyCollection/DotpayBundle/SymfonyCollectionDotpayBundle.php
+++ src/SymfonyCollection/DotpayBundle/SymfonyCollectionDotpayBundle.php
@@ -5,6 +5,7 @@ namespace SymfonyCollection\DotpayBundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use SymfonyCollection\DotpayBundle\DependencyInjection\Compiler\CredentialsCompilerPass;
+use SymfonyCollection\DotpayBundle\DependencyInjection\Compiler\SettingsCompilerPass;
 use SymfonyCollection\DotpayBundle\DependencyInjection\Compiler\ValidatorsCompilerPass;
 
 class SymfonyCollectionDotpayBundle extends Bundle
@@ -12,5 +13,7 @@ class SymfonyCollectionDotpayBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         $container->addCompilerPass(new CredentialsCompilerPass());
+        $container->addCompilerPass(new SettingsCompilerPass());
+
     }
 }
