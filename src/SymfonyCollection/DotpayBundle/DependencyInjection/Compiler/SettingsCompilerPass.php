bad78196790c5e19c412bae6dd71f1556f986ff9
Krzysiek Piasecki <krzysiekpiasecki@gmail.com>
2016-04-07 20:50:30 +0200

    Fix class ns

--- src/SymfonyCollection/DotpayBundle/DependencyInjection/Compiler/SettingsCompilerPass.php
+++ src/SymfonyCollection/DotpayBundle/DependencyInjection/Compiler/SettingsCompilerPass.php
@@ -5,20 +5,21 @@ namespace SymfonyCollection\DotpayBundle\DependencyInjection\Compiler;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\Definition;
+use SymfonyCollection\DotpayBundle\Dotpay\Settings;
 
 /**
  * SettingsCompilerPass
  */
 class SettingsCompilerPass implements CompilerPassInterface
 {
-    
+
     /**
      * @param ContainerBuilder $container
      */
     public function process(ContainerBuilder $container)
     {
         $settings = new Definition(
-            'SymfonyCollection\DotpayBundle\Dotpay\DotpaySettings', [
+            Settings::class, [
                 $container->getParameter('dotpay.ip')
             ]
         );
